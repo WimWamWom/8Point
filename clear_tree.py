f7d77df70486701ac0386f1eebdad43f2d9ca8cc
WimWamWom <wimroehner@gmail.com>
2025-03-21 13:52:22 +0100

    clear_tree.py

---
+++ clear_tree.py
@@ -0,0 +1,32 @@
+import discord
+from discord import app_commands
+import os
+from dotenv import load_dotenv
+
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
+guild_ids_str = str(os.getenv('guildIDs'))
+guild_ids = guild_ids_str.split(',')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+
+
+
+@client.event
+async def on_ready():
+    print("Synchronisiere neue Befehle...")
+    i = 1
+    for guild_id in guild_ids:
+            await tree.sync(guild=discord.Object(id=int(guild_id)))
+            print(f"synchronisiert für Server {i}")
+            i += 1 
+    print("Befehle wurden Synchronisiert! \n============================== \nDer Bot ist Bereit!")
+
+
+client.run(os.getenv('bot_token'))
+
